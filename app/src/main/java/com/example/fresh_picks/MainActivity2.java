30d3f23e350e7073166d149f5138a39946f1bbbc
hanan hijaze <hananhij11@gmail.com>
2024-10-06 10:39:55 +0300

    working on layout for home and first page

--- app/src/main/java/com/example/fresh_picks/MainActivity2.java
+++ app/src/main/java/com/example/fresh_picks/MainActivity2.java
@@ -1,9 +1,6 @@
 package com.example.fresh_picks;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,23 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        // Wait 3 seconds and then start the next activity
-        new Handler(Looper.getMainLooper()).postDelayed(() -> {
-            Intent intent = new Intent(MainActivity.this, MainActivity2.class); // Replace MainActivity2.class with the target activity
-            startActivity(intent);
-            finish(); // Optional: Finish the current activity
-        }, 3000); // 3 seconds delay (3000 milliseconds)
     }
 }
